76e83daa0201d2a99e71d724303ed5efc0d34791
olihafsteinn <olihafsteinnh@gmail.com>
2023-10-26 19:17:24 +0000

    commit

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
 "use strict";
-const characterListContainer = document.getElementById("characterList");
-const image = document.getElementById("image");
-fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json) => {
-    console.log(json);
-    //   console.log(json.results);
-    //   console.log(json.info)
-    json.results.forEach(result => {
-        const characterDiv = document.createElement("div");
-        characterDiv.innerHTML = result.name, result.episode;
-        characterListContainer === null || characterListContainer === void 0 ? void 0 : characterListContainer.appendChild(characterDiv);
+document.addEventListener("DOMContentLoaded", function () {
+    const characterListContainer = document.getElementById("characterList");
+    // const image = document.getElementById("image"); eða seinna
+    fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json) => {
+        console.log(json);
+        json.results.forEach(result => {
+            const characterDiv = document.createElement("div");
+            // characterDiv.innerHTML = `${result.name} - Appears in ${result.episode.length} episodes `;
+            characterDiv.innerHTML = `
+                <div class="test">
+                    <div>
+                        <img src="${result.image}" alt="${result.name}">
+                    </div>
+
+                    <div class="characterInfo">
+                        <h2>${result.name}</h2>
+                        <ul>
+                            <li>${result.gender}</li>
+                            <li>${result.species}</li>
+                            <li>${result.status}</li>
+                            <li>${result.episode.length} episodes</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+            characterListContainer === null || characterListContainer === void 0 ? void 0 : characterListContainer.appendChild(characterDiv);
+        });
+        const valueToCount = "Alien";
+        let countAliens = 0;
+        for (let i = 0; i < json.results.length; i++) {
+            if (json.results[i].species === valueToCount) {
+                countAliens++;
+            }
+        }
+        console.log("There are " + countAliens + " aliens in this database");
+        for (let i = 0; i < json.results.length; i++) {
+            if (json.results[i].status === "Alive") {
+                console.log(json.results[i].name + " is alive");
+            }
+        }
     });
-    //   for (let i = 0; i < json.results.length; i++) {       // Loops through all the characters
-    //     console.log(json.results[i].name + " appears in " + json.results[i].episode.length + " episodes")
-    //   }
-    // Alien counter
-    const valueToCount = "Alien";
-    let countAliens = 0;
-    for (let i = 0; i < json.results.length; i++) {
-        if (json.results[i].species === valueToCount) {
-            countAliens++;
-        }
-    }
-    console.log("There are " + countAliens + " aliens in this database");
-    for (let i = 0; i < json.results.length; i++) {
-        if (json.results[i].status === "Alive") {
-            console.log(json.results[i].name + " is alive");
-        }
-    }
 });
--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-const characterListContainer = document.getElementById("characterList");
-const image = document.getElementById("image")
-
 type Info = {
     count: number,
     next: string | null,
@@ -28,38 +25,51 @@ type Character = {
     url: string,
     }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const characterListContainer = document.getElementById("characterList");
+    // const image = document.getElementById("image"); eða seinna
 
-fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json:{info:Info, results:Character[] }) => {
+    fetch('https://rickandmortyapi.com/api/character').then((r) => r.json()).then((json: { info: Info, results: Character[] }) => {
+        console.log(json);
 
-console.log(json);
-//   console.log(json.results);
-//   console.log(json.info)
-    
-json.results.forEach(result => {
-    const characterDiv = document.createElement("div");
-    characterDiv.innerHTML = result.name
-    characterListContainer?.appendChild(characterDiv);
-})
+        json.results.forEach(result => {
+            const characterDiv = document.createElement("div");
+            // characterDiv.innerHTML = `${result.name} - Appears in ${result.episode.length} episodes `;
+            characterDiv.innerHTML = `
+                <div class="test">
+                    <div>
+                        <img src="${result.image}" alt="${result.name}">
+                    </div>
 
+                    <div class="characterInfo">
+                        <h2>${result.name}</h2>
+                        <ul>
+                            <li>${result.gender}</li>
+                            <li>${result.species}</li>
+                            <li>${result.status}</li>
+                            <li>${result.episode.length} episodes</li>
+                        </ul>
+                    </div>
+                </div>
+                `;
+            characterListContainer?.appendChild(characterDiv);
+        });
 
-//   for (let i = 0; i < json.results.length; i++) {       // Loops through all the characters
-//     console.log(json.results[i].name + " appears in " + json.results[i].episode.length + " episodes")
-//   }
+        const valueToCount: string = "Alien";
+        let countAliens: number = 0;
 
-// Alien counter
-const valueToCount:string = "Alien";
-let countAliens:number = 0;
-for (let i = 0; i < json.results.length; i++) {
-    if (json.results[i].species === valueToCount) {
-        countAliens++;
-    }
-}
-console.log("There are " + countAliens + " aliens in this database")
+        for (let i = 0; i < json.results.length; i++) {
+            if (json.results[i].species === valueToCount) {
+                countAliens++;
+            }
+        }
 
-for (let i = 0; i < json.results.length; i++) {
-    if (json.results[i].status === "Alive") {
-        console.log(json.results[i].name + " is alive")
-    } 
+        console.log("There are " + countAliens + " aliens in this database");
 
+        for (let i = 0; i < json.results.length; i++) {
+            if (json.results[i].status === "Alive") {
+                console.log(json.results[i].name + " is alive");
+            }
+        }
+    });
 });
-
